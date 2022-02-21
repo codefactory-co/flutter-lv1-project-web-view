40e34c84cfaa4568d552e015e0dfd8c316b39513
Ji Ho Choi <jihochoi@Jiui-MacBookPro.local>
2022-02-21 13:17:33 +0900

    done

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:web_view/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  WebViewController? controller;
+  final homeUrl = 'https://blog.codefactory.ai';
+
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Code Factory'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              // http://
+              // https://
+              if(controller == null){
+                return;
+              }
+
+              controller!.loadUrl(homeUrl);
+            },
+            icon: Icon(
+              Icons.home,
+            ),
+          ),
+        ],
+      ),
+      body: WebView(
+        onWebViewCreated: (WebViewController controller) {
+          this.controller = controller;
+        },
+        initialUrl: homeUrl,
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
